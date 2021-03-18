e56bf56f3a63cc8452229e4fe12712859a84732a
Whitney Svoboda <whitney.svoboda@gmail.com>
2021-03-18 10:08:37 -0500

    commits first draft of messages

--- goodVSBadGame.py
+++ goodVSBadGame.py
@@ -1,1 +1,18 @@
-# testingggggggg
+# message to users when opening game
+
+def openingMessage():
+    print("Welcome to the dungeon! Prepare yourself to battle the troll!")
+    goodGuyName = input("What is your name, warrior? ")
+    print(f"Good luck, {goodGuyName}.\n")
+
+def battleMessage():
+    print("You have 10 health points. The troll has 10 health points.")
+    print("""
+    Choose from the following options:
+    1. Fight the troll
+    2. Do nothing
+    3. Run away
+    """)
+
+openingMessage()
+battleMessage()
